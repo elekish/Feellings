4c0dab7d80bd49c3aaff0892d28a062fd97426df
Ishita <144627073+elekish@users.noreply.github.com>
2023-11-04 02:20:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from transformers import pipeline, GPT2LMHeadModel, GPT2Tokenizer
 # Load pre-trained sentiment analysis model
 sentiment_analyzer = pipeline('sentiment-analysis', framework='pt')
 
-# Load pre-trained GPT-2 model and tokenizer with explicit framework
-gpt2_model_name = "gpt2"  # You can replace this with a fine-tuned model if available
 
-# Specify the framework you want to use (e.g., 'pt' for PyTorch or 'tf' for TensorFlow)
+gpt2_model_name = "gpt2"  
+
+
 gpt2_model = GPT2LMHeadModel.from_pretrained(gpt2_model_name)
 gpt2_tokenizer = GPT2Tokenizer.from_pretrained(gpt2_model_name, framework='pt')
 
@@ -16,11 +16,10 @@ st.title(" E-Therapist ")
 
 user_input = st.text_input("Enter your message:")
 if st.button("Submit"):
-    # Analyze the sentiment of the user input
+
     sentiment_result = sentiment_analyzer(user_input)
     predicted_sentiment = sentiment_result[0]['label']
 
-    # Generate a response based on sentiment
     input_text = f"Sentiment: \n{predicted_sentiment}\nTherapist:"
     input_ids = gpt2_tokenizer.encode(input_text, return_tensors="pt")
 
